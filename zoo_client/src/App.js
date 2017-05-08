e81fcaf4746c5adcbf9b038e54ed46af55e75b41
Hawk94 <x99tom.miller@gmail.com>
2017-05-08 22:18:23 +0100

    React todo running

--- zoo_client/src/App.js
+++ zoo_client/src/App.js
@@ -1,52 +1,22 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-// import Home from './Home.js';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import TodoList from './to_do';
+import './App.css';
 
-var Home = React.createClass({
-  render: function() {
-      return (
-        <div>
-          <h2>HELLO</h2>
-          <p>Cras facilisis urna ornare ex volutpat, et
-          convallis erat elementum. Ut aliquam, ipsum vitae
-          gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-          metus nec massa. Maecenas hendrerit laoreet augue
-          nec molestie. Cum sociis natoque penatibus et magnis
-          dis parturient montes, nascetur ridiculus mus.</p>
-  
-          <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-        </div>
-      )
-    }
-    });
-
-var App = React.createClass({
-  render: function() {
+class App extends Component {
+  render() {
     return (
-    <div>
-      <h1>The Zoo.</h1>
-      <ul className="header">
-        <li><Link to="/" activeClassName="active">Home</Link></li>
-        <li><Link to="/stuff" activeClassName="active">Stuff</Link></li>
-        <li><Link to="/contact" activeClassName="active">Contact</Link></li>
-      </ul>
-
-      <hr/>
-
-      <div className='content'>
-        <h2>HELLO</h2>
-        <p>Cras facilisis urna ornare ex volutpat, et
-        convallis erat elementum. Ut aliquam, ipsum vitae
-        gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-        metus nec massa. Maecenas hendrerit laoreet augue
-        nec molestie. Cum sociis natoque penatibus et magnis
-        dis parturient montes, nascetur ridiculus mus.</p>
-
-        <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>ToDo</h2>
+        </div>
+        <p className="App-intro">
+          <TodoList/>
+          </p>
       </div>
-
-    </div>
-  )
+    );
+  }
 }
-});
-export default App
+
+export default App;
